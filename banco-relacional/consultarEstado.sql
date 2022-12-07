4a8fa9ba1bd42ae39b999d4bd7817fb2f6a42615
NikolasDaltro <nikolasdsantos@gmail.com>
2022-12-07 16:54:35 -0300

    Consultar Estado

---
+++ banco-relacional/consultarEstado.sql
@@ -0,0 +1,8 @@
+select * from estados
+
+select sigla, nome as 'Nome dos Estados' from estados
+where regiao = 'Sul'
+
+select nome, regiao, populacao from estados
+where populacao >= 10
+order by populacao desc

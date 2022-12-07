fa22a59dbaf595b832cd4fb124b004ecd15bfc92
NikolasDaltro <nikolasdsantos@gmail.com>
2022-12-07 17:43:14 -0300

    excluindo estados teste

---
+++ banco-relacional/excluirEstados.sql
@@ -0,0 +1,8 @@
+delete from estados
+where sigla = 'MN';
+
+select * from estados
+
+delete from estados
+where id  >= 1000
+
--- banco-relacional/inserirNovoEstado.sql
+++ banco-relacional/inserirNovoEstado.sql
@@ -1,7 +1,7 @@
 insert into estados (id, nome , sigla, regiao, populacao) --com id
-values(1000, 'Novo', 'NV', 'sul', 3.10)
-
-select * from estados
+values(1000, 'Novo', 'NV', 'sul', 3.10);
 
 insert into estados (nome, sigla, regiao, populacao) -- sem id
 values('Mais Novo', 'MN', 'Norte', 2.99)
+
+select * from estados

aa00fa07b87700ce99d562f95df4ecf66160cde8
NikolasDaltro <nikolasdsantos@gmail.com>
2022-12-08 11:45:06 -0300

    inserindo prefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,13 @@
+INSERT INTO prefeitos
+(nome, cidade_id)
+VALUES
+ ('Rodrigo Neves', 3),
+ ('Raquel Lira', 4),
+ ('Zenaldo Coutinho', NULL);
+
+INSERT INTO prefeitos
+(nome, cidade_id)
+VALUES
+('Rafael Greca', NULL);
+
+select * from prefeitos
